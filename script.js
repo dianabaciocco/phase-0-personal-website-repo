707ca00da44d8f3bb1734618f2a20590b0d94249
Diana <dianabacioccoart@gmail.com>
2021-09-16 13:28:16 -0700

    Create inital HTML content

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+// mouseover
+// click
